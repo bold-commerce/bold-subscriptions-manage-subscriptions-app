6f87d7c15f9021563ec43a3738873ed131993e5a
Passion Dev <az.fullstack2258@gmail.com>
2019-09-09 22:52:04 -0500

    Add update orders ship date saga

--- src/sagas/orderNextShipDateSagas.js
+++ src/sagas/orderNextShipDateSagas.js
@@ -1,4 +1,5 @@
-import { all, call, put, takeLatest } from 'redux-saga/effects';
+import { all, call, put, takeLatest, select } from 'redux-saga/effects';
+import moment from 'moment';
 
 import * as requestHelpers from '../helpers/requestHelpers';
 import * as types from '../constants/actionTypes';
@@ -20,12 +21,52 @@ export function* orderUpdateNextShipDate(action) {
   }
 }
 
+export function* updateOrdersNextShipDate() {
+  yield put(actions.updatingOrdersNextShipDate());
+
+  const state = yield select();
+
+  const invalidOrderIds = [];
+  const nextOrderDates = [];
+  state.data.orders.forEach((order) => {
+    const purDate = moment(order.purchase_date);
+    const { interval_type: intervalType } = order.order_interval_type;
+    if (intervalType === 'month') {
+      const nextOrderDate =
+        purDate
+          .add(order.interval_number, 'months')
+          .subtract(5, 'day')
+          .format('YYYY-MM-DD');
+      if (nextOrderDate !== order.next_orders[0]) {
+        invalidOrderIds.push(order.id);
+        nextOrderDates.push(nextOrderDate);
+      }
+    }
+  });
+
+  if (invalidOrderIds.length) {
+    yield all(
+      invalidOrderIds.map(
+        (orderId, i) =>
+          call(orderUpdateNextShipDate, { payload: { orderId, nextShipDate: nextOrderDates[i] } }),
+      ),
+    );
+  }
+
+  yield put(actions.updatedOrdersNextShipDate());
+}
+
 function* watchOrderUpdateNextShipDate() {
   yield takeLatest(types.ORDER_UPDATE_NEXT_SHIP_DATE, orderUpdateNextShipDate);
 }
 
+function* watchUpdateOrdersNextShipDate() {
+  yield takeLatest(types.UPDATE_ORDERS_NEXT_SHIP_DATE, updateOrdersNextShipDate);
+}
+
 export default function* orderNextShipDateSagas() {
   yield all([
+    watchUpdateOrdersNextShipDate(),
     watchOrderUpdateNextShipDate(),
   ]);
 }
